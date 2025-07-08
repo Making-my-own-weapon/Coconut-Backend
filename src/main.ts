74d1b77a36caf7d1f3c7473e6acad26c9099f511
BJJ <a01072004146@gmail.com>
2025-07-08 14:45:38 +0900

    Chore(#30): .env를 통한 url 진입

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3005',
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   });
 
