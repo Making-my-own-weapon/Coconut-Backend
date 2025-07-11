8830aa6bf079c0ebaa76ec89267b147b2baf6566
이의재(학부생-나노전자물리학과) <luj0616@kookmin.kr>
2025-07-11 21:44:12 +0900

    feat(#88): 문제 패널 기능 개발

--- src/problems/problems.module.ts
+++ src/problems/problems.module.ts
@@ -6,9 +6,13 @@ import { Problem } from './entities/problem.entity';
 import { RoomProblem } from './entities/room-problem.entity';
 import { Testcase } from './entities/testcase.entity';
 import { Room } from '../rooms/entities/room.entity'; // ← 추가
+import { EditorModule } from '../editor/editor.module'; // ← 추가
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Problem, RoomProblem, Testcase, Room])],
+  imports: [
+    TypeOrmModule.forFeature([Problem, RoomProblem, Testcase, Room]),
+    EditorModule,
+  ], // ← EditorModule 추가
   controllers: [ProblemsController],
   providers: [ProblemsService],
 })
