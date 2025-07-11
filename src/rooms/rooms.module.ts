cbdc3ee50f32e976fc7a7f5474c01535b88acf73
syounng <stellaj0522@gmail.com>
2025-07-11 23:13:08 +0900

    fix(#43): fix typo

--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -6,9 +6,10 @@ import { RoomsController } from './rooms.controller';
 import { Room } from './entities/room.entity';
 import { RoomProblem } from '../problems/entities/room-problem.entity';
 import { Problem } from '../problems/entities/problem.entity';
+import { Submission } from '../submissions/entities/submission.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Room, RoomProblem, Problem])],
+  imports: [TypeOrmModule.forFeature([Room, RoomProblem, Problem, Submission])],
   controllers: [RoomsController],
   providers: [RoomsService],
   exports: [RoomsService],
