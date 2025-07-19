dc8d8e859d88951d86eede964268ea3b03b2a9f8
syounng <stellaj0522@gmail.com>
2025-07-20 00:33:26 +0900

    fix: 헬스체크 엔드포인트 인가 없이 접근

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('health')
 export class HealthController {
   @Get()
+  @Public()
   check(@Res() res: Response) {
     res.status(200).send('healthy\n');
   }
